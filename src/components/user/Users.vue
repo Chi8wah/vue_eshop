26854a96584a4531f9e4ebf91b57880a1ca8979f
Chiwah <986771093@qq.com>
2020-07-29 10:41:37 +0800

    增加删除用户功能

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -70,7 +70,7 @@
 <!--            修改按钮-->
             <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
 <!--            删除按钮-->
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
 <!--            分配角色按钮-->
             <el-tooltip
               effect="dark"
@@ -353,6 +353,24 @@ export default {
         // 提示修改成功
         this.$message.success('更新用户信息成功')
       })
+    },
+    // 根据id删除对应用户
+    async removeUserById (id) {
+      // 弹框确认
+      const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('已取消删除')
+      }
+      const { data: res } = await this.$http.delete('users/' + id)
+      if (res.meta.status !== 200) {
+        return this.$message.error('删除用户失败')
+      }
+      this.$message.success('删除用户成功')
+      this.getUserList()
     }
   }
 }
