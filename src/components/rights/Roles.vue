87bc0f6c1c9030140926ea61248b17e85fc9c50d
Chiwah <986771093@qq.com>
2020-08-19 17:59:38 +0800

    完成角色下权限列表渲染

--- src/components/rights/Roles.vue
+++ src/components/rights/Roles.vue
@@ -19,7 +19,33 @@
 <!--        角色列表区域-->
         <el-table :data="roleList" border stripe>
 <!--          展开列-->
-          <el-table-column type="expand"></el-table-column>
+          <el-table-column type="expand">
+            <template slot-scope="scope">
+              <el-row :class="['bdbottom', i1 === 0 ? 'bdtop' : '', 'vcenter']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
+<!--                渲染一级权限-->
+                <el-col :span="5">
+                  <el-tag>{{item1.authName}}</el-tag>
+                  <i class="el-icon-caret-right"></i>
+                </el-col>
+<!--                渲染二级和三级权限-->
+                <el-col :span="19">
+<!--                  通过for循环嵌套渲染二级权限-->
+                  <el-row :class="[i2===0?'':'bdtop', 'vcenter']" v-for="(item2, i2) in item1.children" :key="item2.id">
+                    <el-col :span="6">
+                      <el-tag type="success">{{item2.authName}}</el-tag>
+                      <i class="el-icon-caret-right"></i>
+                    </el-col>
+                    <el-col :span="18">
+                      <el-tag type="warning" v-for="(item3) in item2.children" :key="item3.id">
+                        {{item3.authName}}
+                      </el-tag>
+                    </el-col>
+                  </el-row>
+                </el-col>
+
+              </el-row>
+            </template>
+          </el-table-column>
 <!--          索引列-->
           <el-table-column type="index"></el-table-column>
           <el-table-column label="角色名称" prop="roleName"></el-table-column>
@@ -189,5 +215,17 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .el-tag{
+    margin: 7px;
+  }
+  .bdtop{
+    border-top: 1px solid #eee;
+  }
+  .bdbottom{
+    border-bottom: 1px solid #eee;
+  }
+  .vcenter{
+      display: flex;
+    align-items: center;
+  }
 </style>
