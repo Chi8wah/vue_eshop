1f4c441339b9c635808b97351063132be390b45f
Chiwah <986771093@qq.com>
2020-08-30 18:12:34 +0800

    完成商品分类界面渲染

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+<!--    面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+<!--    卡片视图区-->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加分类</el-button>
+        </el-col>
+      </el-row>
+<!--      表格-->
+      <tree-table
+        :data="catelist"
+        :columns="columns"
+        :selection-type="false"
+        :expand-type="false"
+        :show-index="true"
+        index-text="#"
+        :border="true"
+        :show-row-hover="false"
+      >
+        <template slot="isok" slot-scope="scope">
+          <i class="el-icon-success" v-if="scope.row.cat_deleted === false" style="color: lightgreen"></i>
+          <i class="el-icon-error" style="color: red" v-else></i>
+        </template>
+      </tree-table>
+<!--      分页区-->
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cate',
+  data () {
+    return {
+      // 查询条件
+      querInfo: {
+        type: 3,
+        pagenum: 1,
+        pagesize: 5
+      },
+      // 商品分类列表
+      catelist: [],
+      // 总数据条数
+      total: 0,
+      columns: [{
+        label: '分类名称',
+        prop: 'cat_name'
+      }, {
+        label: '是否有效',
+        // 将当前列定义为模板列
+        type: 'template',
+        // 表示当前列使用的模板名称
+        template: 'isok'
+      }]
+    }
+  },
+  created () {
+    this.getCateList()
+  },
+  methods: {
+    // 获取商品分类数据
+    async getCateList () {
+      const { data: res } = await this.$http.get('categories', {
+        params: this.querInfo
+      })
+      if (res.meta.status !== 200) {
+        this.$message.error('获取商品分类失败')
+      }
+      // 将获得的数据赋值给对应变量
+      this.catelist = res.data.result
+      this.total = res.data.total
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
